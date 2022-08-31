a963814ee2847dcd1788fb30d1208a06ca038ba7
MasonDMitchell <mason.mitchellzone@gmail.com>
2022-08-30 21:39:32 -0400

    Initial commit

---
+++ src/Robot.m
@@ -0,0 +1,103 @@
+classdef Robot < handle
+    
+    properties
+        myHIDSimplePacketComs;
+        pol; 
+        GRIPPER_ID = 1962
+    end
+    
+    methods
+        
+        %The is a shutdown function to clear the HID hardware connection
+        function  shutdown(self)
+	    %Close the device
+            self.myHIDSimplePacketComs.disconnect();
+        end
+        
+        % Create a packet processor for an HID device with USB PID 0x007
+        function self = Robot(dev)
+             self.myHIDSimplePacketComs=dev; 
+            self.pol = java.lang.Boolean(false);
+        end
+        
+        %Perform a command cycle. This function will take in a command ID
+        %and a list of 32 bit floating point numbers and pass them over the
+        %HID interface to the device, it will take the response and parse
+        %them back into a list of 32 bit floating point numbers as well
+        function com = command(self, idOfCommand, values)
+                com= zeros(15, 1, 'single');
+                try
+                    ds = javaArray('java.lang.Double',length(values));
+                    for i=1:length(values)
+                        ds(i)= java.lang.Double(values(i));
+                    end
+                    % Default packet size for HID
+                    intid = java.lang.Integer(idOfCommand);
+                    self.myHIDSimplePacketComs.writeFloats(intid,  ds);
+                    ret = 	self.myHIDSimplePacketComs.readFloats(intid) ;
+                    for i=1:length(com)
+                       com(i)= ret(i).floatValue();
+                    end
+                catch exception
+                    getReport(exception)
+                    disp('Command error, reading too fast');
+                end
+        end
+        
+        function com = read(self, idOfCommand)
+                com= zeros(15, 1, 'single');
+                try
+
+                    % Default packet size for HID
+                    intid = java.lang.Integer(idOfCommand);
+                    ret = 	self.myHIDSimplePacketComs.readFloats(intid) ;
+                    for i=1:length(com)
+                       com(i)= ret(i).floatValue();
+                    end
+                catch exception
+                  getReport(exception)
+                    disp('Command error, reading too fast');
+                end
+        end
+        
+        function  write(self, idOfCommand, values)
+                try
+                    ds = javaArray('java.lang.Double',length(values));
+                    for i=1:length(values)
+                        ds(i)= java.lang.Double(values(i));
+                    end
+                    % Default packet size for HID
+                    intid = java.lang.Integer(idOfCommand);
+                    self.myHIDSimplePacketComs.writeFloats(intid,  ds,self.pol);
+
+                catch exception
+                    getReport(exception)
+                    disp('Command error, reading too fast');
+                end
+        end
+        
+        % Specifies a position to the gripper
+        function writeGripper(self, value)
+            try
+                ds = javaArray('java.lang.Byte',length(1));
+                ds(1)= java.lang.Byte(value);
+                intid = java.lang.Integer(self.GRIPPER_ID);
+                self.myHIDSimplePacketComs.writeBytes(intid, ds, self.pol);
+            catch exception
+                getReport(exception)
+                disp('Command error, reading too fast');
+            end
+        end
+        
+        % Opens the gripper
+        function openGripper(self)
+            self.writeGripper(180);
+        end
+        
+        % Closes the gripper
+        function closeGripper(self)
+            self.writeGripper(0);
+        end
+        
+    end
+end
