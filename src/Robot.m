5be4c8e6ad6f925724fd672aed1b34f211f2ab35
Lehong-Wang <lwang11@wpi.edu>
2022-09-18 16:03:53 -0400

    add file

--- src/Robot.m
+++ src/Robot.m
@@ -3,178 +3,15 @@ classdef Robot < handle
     properties
         myHIDSimplePacketComs;
         pol; 
-        GRIPPER_ID = 1962;
-        SERV_ID = 1848; 
-%         matrix = Matrix();
+        jointSetpointGoal;
+        SERV_ID = 1848;
+        SERVER_ID_READ =1910;
+        VEL_ID = 1822;
+        GRIPPER_ID = 1962
     end
-%     matrix = Matrix();
-
-    methods
     
-% ######################################## Lab 3 Functions ########################################
-        % takes a 3x1 task space position vector (i.e. x, y, z components of pBaseTip that 
-        % is the robot end-effector’s position w.r.t the base frame) as the input and returns a set
-        % of corresponding joint angles (i.e. q1, q2, q3) that would make the robot’s end-effector 
-        % move to that target position. 
-        function T = ik3001(matrix)
-            % Figure out what q1, q2, q3 are
-            q1 = atan2(0);
-            q2 = atan2(0);
-            q3 = atan2(0);
-            T = [q1 q2 q3];
-            disp(T);
-        end
+    methods
         
-        % Takes two arguments, trajectory coefficients and the total maount
-        % of time the trajector should take
-        function T = run_trajectory(coeff, duration)
-        end
-
-
-% ######################################## Lab 2 Functions ########################################
-
-        % return transformation matrix of base to tip
-        function T = measured_cp(self)
-            status_tab = self.measured_js(1,0);
-            pos = status_tab(1,:);
-            % turn degree to radians
-            pos_rad = arrayfun(@(x) deg2rad(x), pos);
-            T = Matrix.fk3001(pos_rad);
-            disp(pos);
-        end
-
-        % takes data from setpoint_js() and returns a 4x4 homogeneous transformation 
-        % matrix based upon the current joint set point positions in degrees.
-        function T = setpoint_cp(self)
-            set_tab = self.setpoint_js();
-            set_rad = arrayfun(@(x) deg2rad(x), set_tab);
-            T = Matrix.fk3001(set_rad);
-%             disp(set_rad);
-        end
-
-        %  takes  data  from  goal_js()  and  returns  a  4x4  homogeneous  transformation 
-        % matrix based upon the commanded end of motion joint set point positions in degrees.  
-        function T = goal_cp(self)
-            goal_tab = self.goal_js();
-            goal_rad = arrayfun(@(x) deg2rad(x), goal_tab);
-            T = Matrix.fk3001(goal_rad);
-%             disp(goal_rad);
-        end
-
-        % return all transformation matrix of the joints
-        % helper for ploting
-        % should be 4*4*n matrix (n=4)
-        function T = get_all_trans_mat(self)
-            T_0 = zeros(4);
-            status_tab = self.measured_js(1,0);
-            joint_angle = status_tab(1,:);
-            % turn degree to radians
-            pos_rad = arrayfun(@(x) deg2rad(x), joint_angle);
-            T_1 = Matrix.fk3001(zeros(0));
-            T_2 = Matrix.fk3001(pos_rad(1));
-            T_3 = Matrix.fk3001(pos_rad(1:2));
-            T_4 = Matrix.fk3001(pos_rad(1:3));
-
-            T(:,:,1) = T_0;
-            T(:,:,2) = T_1;
-            T(:,:,3) = T_2;
-            T(:,:,4) = T_3;
-            T(:,:,5) = T_4;
-
-
-        end
-
-
-% ######################################## Lab 1 Functions ########################################
-
-
-        % Returns  a  1x3  array  that  contains  the  end-of-motion  joint  
-        % setpoint  positions  in degrees.
-	    function packet = goal_js(self)
-            packet = zeros(1,3, 'single');      % Initalize the matrix to zeros
-            SERVER_ID_READ = 1848; % Set Motor Setpoints with Time (Interpolation) packet ID
-            returnPacket = self.read(SERVER_ID_READ); % Read from Set Motor Setpoints with Time (Interpolation) packet
-            % Set corresponding packet data to correct matrix row
-            packet(1,1) = returnPacket(3);
-            packet(1,2) = returnPacket(4);
-            packet(1,3) = returnPacket(5);
-            disp(packet);
-        end
-
-        % Which takes  two  boolean  values, named  GETPOS  and GETVEL. Only  
-        % return the  results  for the requested data, and set the rest to
-        % zero.
-        % Which returns a 2x3 array that contains current joint positions in degrees (1st row) and/or 
-        % current joint velocities (2nd row).
-	    function out = measured_js(self, GETPOS, GETVEL)
-            out = zeros(2,3,'single');  % Initalize matrix to zeros
-            if GETPOS   % If we want the position
-                pos = self.read(1910); % Read from Positions and Setpoint packet
-                % Set corresponding packet data to the matrix
-                out(1,1) = pos(3);
-                out(1,2) = pos(5);
-                out(1,3) = pos(7);
-            end 
-            
-            if GETVEL   % If we want the velocity
-                pos = self.read(1822); % Read from Velocity data packet
-                % Set corresponding packet data to the matrix
-                out(2,1) = pos(3);
-                out(2,2) = pos(6);
-                out(2,3) = pos(9);
-            end
-%             disp(out);    % Display the data
-        end
-
-        % takes a 1x3 array of joint values and an interpolation time in ms to get there 
-        function interpolate_jp(self, targets, time)
-            packet = zeros(15, 1, 'single');        % Initalize matrix to zeros
-            packet(1) = time; % time in ms
-            packet(2) = 0; % linear interpolation
-            packet(3) = targets(1); % First link
-            packet(4) = targets(2); % Second link
-            packet(5) = targets(3); % Third link
-            % Write the packet
-            self.write(self.SERV_ID, packet);
-            return
-        end
-        
-        % takes a 1x3 array of joint values in degrees to be sent directly to the actuators and 
-        % bypasses interpolation 
- 	    function servo_jp(self, targets)
-            packet = zeros(15,1,'single');      % Initalize matrix to zeros
-            % Set corresponding packet data to correct matrix row
-            packet(1) = 0;
-            packet(2) = 0;
-            packet(3) = targets(1); % First link
-            packet(4) = targets(2); % Second link
-            packet(5) = targets(3); % Third link
-            % Write the packet
-            self.write(self.SERV_ID, packet);
-            return
-        end
-        
-        % Returns  a  1x3  array  that  contains  current  joint  set  point  positions  in  degrees.  
-        function packet = setpoint_js(self)
-            packet = zeros(1, 3, 'single');     % Initalize matrix to zeros
-            SERVER_ID_READ = 1848; % Setpoint packet ID
-            returnPacket = self.read(SERVER_ID_READ) % Read from Postions and Setpoint packet
-            packet(1) = returnPacket(3);
-            packet(2) = returnPacket(4);
-            packet(3) = returnPacket(5);
-%             disp("Packet HERE");
-%             disp(packet); % Display the data
-%             disp("END");
-        end
-
-
-
-
-
-% ######################################## Default Functions ########################################
-
-
-
         %The is a shutdown function to clear the HID hardware connection
         function  shutdown(self)
 	    %Close the device
@@ -183,7 +20,7 @@ classdef Robot < handle
         
         % Create a packet processor for an HID device with USB PID 0x007
         function self = Robot(dev)
-            self.myHIDSimplePacketComs=dev;
+             self.myHIDSimplePacketComs=dev; 
             self.pol = java.lang.Boolean(false);
         end
         
@@ -265,6 +102,205 @@ classdef Robot < handle
         function closeGripper(self)
             self.writeGripper(0);
         end
+
+      
+        function servo_jp(packet, positionArray)
+            packet.write(packet.SERV_ID, [0; 0; positionArray.'; zeros(15, 1)] );
+            packet.jointSetpointGoal = positionArray;
+        end
+       
+        function interpolate_jp(packet, positionArray, interpolateTime)
+            packet.write(packet.SERV_ID, [interpolateTime; 0; positionArray.'; zeros(15, 1)] );
+            packet.jointSetpointGoal = positionArray;
+        end
+% measured_js funtion
+% takes two boolean GETPOS and GETVEL and 
+% return a 2x3 array contains current joint positio and velocities
+        function returnArray =  measured_js(packet, GETPOS, GETVEL)
+            position = zeros(1,3);
+            velocity = zeros(1,3);
+            posData = packet.read(packet.SERVER_ID_READ);
+            velData = packet.read(packet.VEL_ID);
+            
+            if GETPOS == 1
+                position = [posData(3) posData(5) posData(7)];
+            end
+            
+            if GETVEL == 1
+                velocity = [velData(3) velData(6) velData(9)];
+            end
+
+            returnArray = [position; velocity];
+        end
+        
+        
+% return a 1x3 array contains current joint set point position in degrees
+% before using the function, it needs to pause for a little while to waite
+% the new setPoint data been written into the packet, or it will return
+% invalid value 
+        function returnSetpoint = setpoint_js(packet)
+            setpointData = packet.read(packet.SERVER_ID_READ);
+            returnSetpoint = [setpointData(2) setpointData(4) setpointData(6)];
+        end
+
+        function returnGoal = goal_js(packet)
+             returnGoal = packet.jointSetpointGoal;
+        end
+
+        %--------------lab2-------------------------
+        % input: a 1*4 array corresponding to a row of the DH parameter
+        % table 
+        % return: a symbolic 4*4 homogeneous transformation
+        % matrix corresponding to that specific row 
+        function returnTM = dh2mat(packet, inputArray)
+            theta = inputArray(1);
+            d = inputArray(2);
+            a = inputArray(3);
+            alpha = inputArray(4);
+
+            tRotZ = [cos(theta), -sin(theta), 0, 0;
+                     sin(theta),  cos(theta), 0, 0;
+                              0,           0, 1, 0;
+                              0,           0, 0, 1];
+            
+            tTransZ = [1,0,0,0;
+                       0,1,0,0;
+                       0,0,1,d;
+                       0,0,0,1];
+
+            tTransX = [1,0,0,a;
+                       0,1,0,0;
+                       0,0,1,0;
+                       0,0,0,1];
+
+            tRotX = [1,          0,           0, 0;
+                     0, cos(alpha), -sin(alpha), 0;
+                     0, sin(alpha),  cos(alpha), 0;
+                     0,          0,           0, 1];
+
+            returnTM = tRotZ * tTransZ * tTransX * tRotX;
+        end
+
+        function returnFKM = dh2fk(packet, inputMatrix, targetFrameIndex)
+            % inputMatrix: corresponding to the n rows of the full DH
+            % parameter table 
+            % returnFKM = T0_4(FK matrix) =  T0_1 * T1_2 * T2_3 * T3_4
+            index = 1;
+            returnFKM = eye(4);
+            while index <= targetFrameIndex
+                currentTM = packet.dh2mat(inputMatrix(index,:));
+                returnFKM = returnFKM * currentTM;
+                index = index+1;
+            end
+            returnFKM = returnFKM;
+        end
+
+        function returnFM = fk3001(packet, jointAnglesArray)
+            % jointAnglesArray = [theta1, theta2, theta3]
+            theta1 = jointAnglesArray(1);
+            theta2 = jointAnglesArray(2);
+            theta3 = jointAnglesArray(3);
+            %{
+            L0 = 55; % Unit: mm
+            L1 = 40; % Unit: mm
+            L2 = 100; % Unit: mm
+            L3 = 100; % Unit: mm
+            inputMatrix = [         0, L0,  0,     0;
+                               theta1, L1,  0, -pi/2;
+                          theta2-pi/2,  0, L2,     0;
+                          theta3+pi/2,  0, L3,     0;];
+            returnFM = packet.dh2fk(inputMatrix, 4);
+            %}
+            returnFM = [ cos((pi*theta1)/180)*cos((pi*(theta2 - 90))/180)*cos((pi*(theta3 + 90))/180) - cos((pi*theta1)/180)*sin((pi*(theta2 - 90))/180)*sin((pi*(theta3 + 90))/180), - cos((pi*theta1)/180)*cos((pi*(theta2 - 90))/180)*sin((pi*(theta3 + 90))/180) - cos((pi*theta1)/180)*cos((pi*(theta3 + 90))/180)*sin((pi*(theta2 - 90))/180), -sin((pi*theta1)/180), 100*cos((pi*theta1)/180)*cos((pi*(theta2 - 90))/180) + 100*cos((pi*theta1)/180)*cos((pi*(theta2 - 90))/180)*cos((pi*(theta3 + 90))/180) - 100*cos((pi*theta1)/180)*sin((pi*(theta2 - 90))/180)*sin((pi*(theta3 + 90))/180);
+                         sin((pi*theta1)/180)*cos((pi*(theta2 - 90))/180)*cos((pi*(theta3 + 90))/180) - sin((pi*theta1)/180)*sin((pi*(theta2 - 90))/180)*sin((pi*(theta3 + 90))/180), - sin((pi*theta1)/180)*cos((pi*(theta2 - 90))/180)*sin((pi*(theta3 + 90))/180) - sin((pi*theta1)/180)*cos((pi*(theta3 + 90))/180)*sin((pi*(theta2 - 90))/180),  cos((pi*theta1)/180), 100*sin((pi*theta1)/180)*cos((pi*(theta2 - 90))/180) + 100*sin((pi*theta1)/180)*cos((pi*(theta2 - 90))/180)*cos((pi*(theta3 + 90))/180) - 100*sin((pi*theta1)/180)*sin((pi*(theta2 - 90))/180)*sin((pi*(theta3 + 90))/180);
+                                                                 - cos((pi*(theta2 - 90))/180)*sin((pi*(theta3 + 90))/180) - cos((pi*(theta3 + 90))/180)*sin((pi*(theta2 - 90))/180),                                             sin((pi*(theta2 - 90))/180)*sin((pi*(theta3 + 90))/180) - cos((pi*(theta2 - 90))/180)*cos((pi*(theta3 + 90))/180),                     0,                                                           95 - 100*cos((pi*(theta2 - 90))/180)*sin((pi*(theta3 + 90))/180) - 100*cos((pi*(theta3 + 90))/180)*sin((pi*(theta2 - 90))/180) - 100*sin((pi*(theta2 - 90))/180);
+                                                                                                                                                                                   0,                                                                                                                                                             0,                     0,                                                                                                                                                                                                                          1];
+        end
+
+        function returnJS = measured_cp(packet)
+            data = packet.measured_js(1, 0);
+            theta1 = data(1,1);
+            theta2 = data(1,2);
+            theta3 = data(1,3);
+
+            returnJS = packet.fk3001([theta1 theta2 theta3]);
+        end
+
+        function returnSP = setpoint_cp(packet)
+            setpointData = packet.setpoint_js();
+            setTheta1 = setpointData(1);
+            setTheta2 = setpointData(2);
+            setTheta3 = setpointData(3);
+
+            returnSP = packet.fk3001([setTheta1 setTheta2 setTheta3]);
+        end
+
+        function returnTM = goal_cp(packet)
+            goalPointData = packet.goal_js();
+            goalTheta1 = goalPointData(1);
+            goalTheta2 = goalPointData(2);
+            goalTheta3 = goalPointData(3);
+
+            returnTM = packet.fk3001([goalTheta1 goalTheta2 goalTheta3]);
+        end
+
+        function returnT = ik3001(packet, positionArray)
+            
+            x = positionArray(1);
+            y = positionArray(2);
+            z = positionArray(3);
+            
+            % check if the given position is within the workspace 
+            if z < 0 || z > 295
+                error("Error: invalid z input");
+            end
+            if x > 180
+                error("Error: invalid x input");
+            end 
+            if y > 200 || y < -200
+                error("Error: invalid y input");
+            end
+       
+       
+            L0 = 55;
+            L1 = 40;
+            a1 = 100;
+            a2 = 100;
+            r = sqrt(x^2 + y^2);
+            s = z - L1 - L0;
+            alpha = atan2(s, r);
+
+            % theta3:
+            D3 = (a1^2+a2^2-(r^2+s^2))/(2*a1*a2);
+            C3 = sqrt(1-D3^2);
+            theta3 = pi/2 - atan2(C3, D3);
+
+            % theta2: 
+            l = realsqrt(r^2+s^2);
+            D2 = (a1^2+r^2+s^2-a2^2)/(2*a1*l);
+            C2 = sqrt(1-D2^2);
+            beta = atan2(C2, D2);
+            theta2 = pi/2 - alpha - beta;
+
+            % theta1:
+            D1 = x/r;
+            C1 = sqrt(1-D1^2);
+            %theta1 = atan2(C1, D1);
+            theta1 = atan2(y, x);
+
+            theta1 = rad2deg(theta1);
+            theta2 = rad2deg(theta2);
+            theta3 = rad2deg(theta3);
+
+            returnT = [theta1; theta2; theta3];
+ 
+        end
+
+
+        
+        
+
+
         
     end
 end
