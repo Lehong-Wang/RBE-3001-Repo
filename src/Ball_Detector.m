2a11eedb95d83546002f0ce2ab7d290809e78235
Lehong-Wang <lwang11@wpi.edu>
2022-10-07 11:11:51 -0400

    modify filter

--- src/Ball_Detector.m
+++ src/Ball_Detector.m
@@ -33,26 +33,30 @@ classdef Ball_Detector
           channel1Min = 0.171;
           channel1Max = 0.215;
 
+          case 'A'
+              channel1Min = 0.9;
+              channel1Max = 0.215;
           otherwise
-           channel1Min = 0;
+          channel1Min = 0;
           channel1Max = 1; 
           warning("Unexpected color type");
 
       end
         % white channel
-      channel2Min = 0.235;
+      channel2Min = 0.3;
       channel2Max = 1.000;
         % black channel
-      channel3Min = 0.35;
+      channel3Min = 0.4;
       channel3Max = 1.000;
 
-%       disp(channel1Min);
-%       disp(channel2Min);
-%       disp(channel3Min);
-
+      disp(mod(channel1Min+0.5, 1));
+      disp(mod(channel1Max+0.5, 1));
+%       disp(I(:,:,1)+0.5);
 
       % Create mask based on chosen histogram thresholds
-      sliderBW = ( (I(:,:,1) >= channel1Min) & (I(:,:,1) <= channel1Max) ) & ...
+      % to deal will red being 0.9-1, 
+      % rotate the sepctrom (+0.5) to get continuous thresholds
+      sliderBW = ( (mod(I(:,:,1)+0.5, 1) >= mod(channel1Min+0.5, 1)) & (mod(I(:,:,1)+0.5, 1) <= mod(channel1Max+0.5, 1)) ) & ...
           (I(:,:,2) >= channel2Min ) & (I(:,:,2) <= channel2Max) & ...
           (I(:,:,3) >= channel3Min ) & (I(:,:,3) <= channel3Max);
       BW = sliderBW;
@@ -78,17 +82,20 @@ classdef Ball_Detector
       % Find connected components.
       blobAnalysis = vision.BlobAnalysis(AreaOutputPort = true,...
           CentroidOutputPort = false,...
-          BoundingBoxOutputPort = true,...
-          MinimumBlobArea = 5000, ExcludeBorderBlobs = true);
-      [areas, boxes] = step(blobAnalysis, BW)
+          BoundingBoxOutputPort = true,...  % get bounding box
+          MinimumBlobArea = 5000,...        % minium area
+          ExcludeBorderBlobs = false);
+      [areas, boxes] = step(blobAnalysis, BW);
       
       % Sort connected components in descending order by area
       [~, idx] = sort(areas, "Descend");
       
-      % Get the two largest components.
+      % get the x,y,w,h of each box (blub)
+      %     x,y is top left of the box
       % boxes = double(boxes(idx(1:2), :))
       boxes = double(boxes(idx, :));
 
+      % center of boxes
       centers = zeros(0);
       for i = (1 : size(boxes,1))
           centers(i,1) = boxes(i,1) + boxes(i,3)/2;
@@ -96,18 +103,19 @@ classdef Ball_Detector
           centers(i,3) = 10;
       end
 
-      % Insert labels for the coins.
-      imDetectedCoins = insertObjectAnnotation(RGB, "rectangle", boxes, "Ball");
-      imDetectedCoins = insertObjectAnnotation(imDetectedCoins, "circle", centers, "Center");
+      % Insert box and center for the balls.
+      labeled_img = insertObjectAnnotation(RGB, "rectangle", boxes, "Ball");
+      labeled_img = insertObjectAnnotation(labeled_img, "circle", centers, "Center");
       figure; 
-      imshow(imDetectedCoins);
-
+      imshow(labeled_img);
+      % get center x,y coord
       centers = centers(:,1:2);
+      disp(centers);
 
     end
 
 
-
+    % use calibration data from Camera class to map balls to base coord
     function reals = getRealCoord(centers, cam)
         cam_height = 180;
         cam_pos = [200, 0];
@@ -116,18 +124,26 @@ classdef Ball_Detector
         reals = zeros(0);
 
         for i = (1 : size(centers,1))
-            pixel_center = centers(i,:)
-            proj_center = cam.getWorldCoord(pixel_center)
+            pixel_center = centers(i,:);
+            proj_center = cam.getWorldCoord(pixel_center);
 
             dis_to_cam = proj_center - cam_pos;
 
-            real_center = proj_center - dis_to_cam * (center_height/cam_height)
+            real_center = proj_center - dis_to_cam * (center_height/cam_height);
             reals(i,:) = real_center;
         end
         disp(reals);
     end
 
 
+
+    % wrapper
+    function coords = getBallPose(img, color, cam)
+        [BW,RGB] = Ball_Detector.createMaskedImage(img, color);
+        centers = Ball_Detector.detectBall(BW, RGB);
+        coords = Ball_Detector.getRealCoord(centers, cam);
+
+    end
 
 
 
